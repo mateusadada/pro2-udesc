b5eb8de5f6740ee765518f01416cabbb2b713abc
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2024-04-23 09:24:46 -0300

    Update Programa_Principal.py
    Removed comments

--- solved_exercises/VPL/1 - Painéis/Programa_Principal.py
+++ solved_exercises/VPL/1 - Painéis/Programa_Principal.py
@@ -2,11 +2,6 @@ import sys
 from PyQt5.QtWidgets import *
 from Janela import Janela
 
-########################################################
-
-## Questão 07: (Verifique se a implementação do programa principal está correta)
 App=QApplication(sys.argv)
 Jan1=Janela("Simulado Painéis")
 App.exec_()
-
-########################################################
