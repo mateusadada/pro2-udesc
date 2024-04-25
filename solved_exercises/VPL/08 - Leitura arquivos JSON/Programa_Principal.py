efc8adc39d7462196b29bb67de40ec08844678b4
Mateus Adada <mateusadada@gmail.com>
2024-04-24 21:11:52 -0300

    Added VPL 7 and 8

---
+++ solved_exercises/VPL/08 - Leitura arquivos JSON/Programa_Principal.py
@@ -0,0 +1,33 @@
+import sys
+import json
+
+# Questão 01: (Crie o código necessário para ler os dados do arquivo JSON
+# O arquivo já é fornecido e possui o nome: "Banco_Alunos.json.txt")
+with open('Banco_Alunos.json.txt', 'r') as f:
+    Data = json.load(f)
+
+# Questão 02: (Crie o código necessário para imprimir na tela os dados das cidades)
+def print_items():
+    output_string = ""
+    for p in Data['Aluno']:
+        print(f'Aluno_Key: { p["Aluno_Key"] }')
+        print(f'Cidade_Key: { p["Cidade_Key"] }')
+        print(f'Aluno_Nome: { p["Aluno_Nome"] }')
+        print(f'Aluno_Idade: { p["Aluno_Idade"] }\n')
+
+    for p in Data['Cidade']:
+        print(f'Cidade_Key: { p["Cidade_Key"] }')
+        print(f'Cidade_Nome: { p["Cidade_Nome"] }')
+        print(f'Cidade_Abrev: { p["Cidade_Abrev"] }\n')
+
+# Questão 03: (Crie o código necessário para imprimir na tela os dados dos alunos)
+try:
+    print_items()
+
+except IOError:
+    print('Erro de acesso aos dados')
+except FileNotFoundError:
+    print('Arquivo não encontrado')
+
+# Questão 04: (crie o código necessário para encerrar o programa)
+sys.exit(0)
