a643a00af58d5d582860294c2160e781e1ad54db
Mateus Adada <mateusadada@gmail.com>
2024-05-24 21:33:49 -0300

    Update VPL 11 official

--- solved_exercises/VPL/11 - Thread Schlachtfest/ThreadSchlacht.py
+++ solved_exercises/VPL/11 - Thread Schlachtfest/ThreadSchlacht.py
@@ -1,31 +1,35 @@
-import random
 import threading
 import time
+
+##################################################
 
 class ThreadSchlacht:
     __Total=None
     __LEd=None
     __Thr=None
 
-    # Questão 01: (Criar o construtor da classe ThreadSchlacht conforme diagrama de classe)
+    ## Questão 01:  (Criar o construtor da classe ThreadSchlacht
+    ##               conforme diagrama da Figura 01)
     def __init__(self, LEd_a):
         self.__LEd = LEd_a
 
     def iniciar(self, Total_a):
-        # Questão 02: (Criar o método que inicia a ThreadSchlacht)
+        ## Questão 02:  (Criar o método que inicia a ThreadSchlacht)
         self.__Total = Total_a
         self.__Thr = threading.Thread(target=self.run)
         self.__Thr.start()
 
     def parar(self):
-        # Questão 03: (Criar o método que encerra a ThreadSchlacht)
+        ## Questão 03:  (Criar o método que encerra a ThreadSchlacht)
         self.__Total = 0
         self.__Thr = None
 
     def run(self):
-        # Questão 04: (Criar o método que realiza a ThreadSchlacht)
+        ## Questão 04:  (Criar o método que realiza a ThreadSchlacht)
         while self.__Total >= 0:
             self.__LEd.setText(str(int(self.__LEd.text()) + 1))
-            time.sleep(random.randint(1,7))
-
+            time.sleep(4)
+            
             self.__Total += -1
+
+##################################################
