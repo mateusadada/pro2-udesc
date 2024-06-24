e6f46819e029d692c034618a367d4d6d9cde5c7b
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2024-06-24 07:45:25 -0300

    Update ThreadAgua.py
    Removed one '#'

--- solved_exercises/VPL/12 - Thread Água/ThreadAgua.py
+++ solved_exercises/VPL/12 - Thread Água/ThreadAgua.py
@@ -7,23 +7,23 @@ class ThreadAgua:
     __Sair=None
     __Thr = None
 
-    ## Questão 01: (Criar o construtor da classe ThreadAgua)
+    # Questão 01: (Criar o construtor da classe ThreadAgua)
     def __init__(self, LEd_a, prod, sair):
         self.__LEd = LEd_a
         self.__Prod_Seg = prod
         self.__Sair = sair
 
     def iniciar(self):
-        ## Questão 02: (Criar o método que inicia a ThreadAgua)
+        # Questão 02: (Criar o método que inicia a ThreadAgua)
         self.__Thr = threading.Thread(target=self.run)
         self.__Thr.start()
 
     def parar(self):
-        ## Questão 03: (Criar o método que encerra a ThreadAgua)
+        # Questão 03: (Criar o método que encerra a ThreadAgua)
         self.__Thr = None
 
     def run(self):
-        ## Questão 04: (Criar o método que realize a ThreadAgua)
+        # Questão 04: (Criar o método que realize a ThreadAgua)
         i = 0
         while True:
             i += 1
