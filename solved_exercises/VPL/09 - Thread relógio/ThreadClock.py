a16cac6062d56d4c14b5c1440e65ab83eda777bf
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2024-07-24 07:34:42 -0300

    Update ThreadClock.py
    Removed '#'

--- solved_exercises/VPL/09 - Thread relógio/ThreadClock.py
+++ solved_exercises/VPL/09 - Thread relógio/ThreadClock.py
@@ -7,13 +7,13 @@ class ThreadClock:
     __LEd = None
     __Thr = None
 
-    ## Questão 01:  (Criar o construtor da classe ThreadClock
-    ##               conforme diagrama da Figura 01)
+    # Questão 01:  (Criar o construtor da classe ThreadClock
+    #               conforme diagrama da Figura 01)
     def __init__(self, LEd):
         self.__LEd = LEd
 
     def iniciar(self, Total_a):
-        ## Questão 02:  (Criar o método que inicia a ThreadClock)
+        # Questão 02:  (Criar o método que inicia a ThreadClock)
         try:
             self.__Total = Total_a
             self.__Thr = threading.Thread(target=self.run)
@@ -22,7 +22,7 @@ class ThreadClock:
             print("Error: unable to start thread: %s\n" % (ex))
 
     def parar(self):
-        ## Questão 03:  (Criar o método que encerra a ThreadClock)
+        # Questão 03:  (Criar o método que encerra a ThreadClock)
         try:
             
             self.__Total = 0;
@@ -31,11 +31,11 @@ class ThreadClock:
             print("Error: unable to start thread: %s\n" % (ex))
 
     def isRunning(self):
-        ## Questão 04:  (Criar o método isRunning)
+        # Questão 04:  (Criar o método isRunning)
         return self.__Thr.is_alive() if self.__Thr else False
 
     def run(self):
-        ## Questão 05:  (Criar o método que realiza a ThreadClock)
+        # Questão 05:  (Criar o método que realiza a ThreadClock)
         count = 0
         while count < self.__Total:
             count += 1
